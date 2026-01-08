b2cc86874aa9cd9a5804963ee813ad0961bafe55
Ira Surati <ira.s@bitontree.com>
2026-01-08 16:00:23 +0530

    Fix: Protected cart route from Admin

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router-dom"
-import ProtectedRoute from "./routes/ProtectedRoute"
 import RoleProtectedRoute from "./routes/RoleProtectedRoute"
 import { USER_ROLES } from "./utils/enums"
 import { ROUTES } from "./utils/routes"
@@ -9,12 +8,9 @@ import AppLayout from "./layout/AppLayout"
 import ProductsPage from "./pages/Products/ProductPage"
 import Register from "./pages/Register/Register"
 import Cart from "./pages/Cart/Cart"
-import AdminPanel from "./pages/Admin/AdminPanel" // import AdminPanel component
+import AdminPanel from "./pages/Admin/AdminPanel"
 
 // TEMP placeholder pages
-//const Login = () => <h2>Login Page</h2>;
-//const Home = () => <h2>Home</h2>;
-const Checkout = () => <h2>Checkout</h2>
 const Unauthorized = () => <h2>Unauthorized</h2>
 
 export default function AppRoutes() {
@@ -25,22 +21,15 @@ export default function AppRoutes() {
       </Route>
       <Route element={<PublicRoute />}>
         <Route path={ROUTES.LOGIN} element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path={ROUTES.REGISTER} element={<Register />} />
       </Route>
 
-      {/* Logged-in users */}
-      <Route element={<ProtectedRoute />}>
-        <Route element={<AppLayout />}>
-          <Route path={ROUTES.CHECKOUT} element={<Checkout />} />
-        </Route>
-      </Route>
-      <Route element={<ProtectedRoute />}>
+      <Route element={<RoleProtectedRoute allowedRoles={[USER_ROLES.USER]} />}>
         <Route element={<AppLayout />}>
           <Route path={ROUTES.CART} element={<Cart />} />
         </Route>
       </Route>
 
-      {/* Admin-only */}
       <Route element={<RoleProtectedRoute allowedRoles={[USER_ROLES.ADMIN]} />}>
         <Route element={<AppLayout />}>
           <Route path={ROUTES.ADMIN} element={<AdminPanel />} />
